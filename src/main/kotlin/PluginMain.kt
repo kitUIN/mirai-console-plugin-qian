6b74f9e6ec1c4a22342066188d0a6b001b6840df
kitUIN <kulujun@gmail.com>
2023-01-12 03:05:02 +0800

    update 0.1.1

--- src/main/kotlin/PluginMain.kt
+++ src/main/kotlin/PluginMain.kt
@@ -27,66 +27,62 @@ import java.time.ZoneId
 import java.util.*
 
 
-object QianConfig : AutoSavePluginConfig("qianConfig"){
+object QianConfig : AutoSavePluginConfig("qianConfig") {
     var error: Boolean = false
     lateinit var qianList: JsonElement
 
 }
-object QianUserTemp:AutoSavePluginData("qianUserTemp"){
-    var historyTemp = mutableMapOf<Long,Int>()
-    var isQian = mutableMapOf<Long,Boolean>()
+
+object QianUserTemp : AutoSavePluginData("qianUserTemp") {
+    var historyTemp = mutableMapOf<Long, Int>()
+    var isQian = mutableMapOf<Long, Boolean>()
 }
+
 class UserTempReload() : TimerTask() {
     override fun run() {
-        QianUserTemp.historyTemp = mutableMapOf<Long,Int>()
-        QianUserTemp.isQian = mutableMapOf<Long,Boolean>()
+        QianUserTemp.historyTemp = mutableMapOf<Long, Int>()
+        QianUserTemp.isQian = mutableMapOf<Long, Boolean>()
     }
 }
 
-fun QianProcess(eventChannel:EventChannel<Event>){
+fun QianProcess(eventChannel: EventChannel<Event>) {
     eventChannel.subscribeAlways<GroupMessageEvent>
     {
-        if(!QianConfig.error){
-            if (message.contentToString() == "抽签")
-            {
-                if(QianUserTemp.isQian[sender.id] == true)
-                {
+        if (!QianConfig.error) {
+            if (message.contentToString() == "抽签") {
+                if (QianUserTemp.isQian[sender.id] == true) {
                     val chain = buildMessageChain {
-                        + message.quote()
-                        + PlainText("今天已经抽过啦(*´﹃｀*)")
+                        +message.quote()
+                        +PlainText("今天已经抽过啦(*´﹃｀*)")
                     }
                     group.sendMessage(chain)
-                }
-                else
-                {
+                } else {
                     val randoms = (1..384).random()
                     val shi = QianConfig.qianList.jsonObject["$randoms"]?.jsonObject?.get("shi").toString()
                     val chain = buildMessageChain {
-                        + message.quote()
-                        + PlainText("[第 $randoms 签]\r\r")
-                        + PlainText(shi)
+                        +message.quote()
+                        +PlainText("[第 $randoms 签]\r\r")
+                        +PlainText(shi)
                     }
                     QianUserTemp.historyTemp[sender.id] = randoms
                     QianUserTemp.isQian[sender.id] = true
                     group.sendMessage(chain)
 
                 }
-            }else if(message.contentToString()=="解签")
-            {
+            } else if (message.contentToString() == "解签") {
                 val randoms = QianUserTemp.historyTemp[sender.id]
-                if (randoms is Int)
-                {
+                if (randoms is Int) {
                     val jie = QianConfig.qianList.jsonObject["$randoms"]?.jsonObject?.get("jie").toString()
                     val chain = buildMessageChain {
-                        + message.quote()
-                        + PlainText("[第 $randoms 签:解签]\r\r")
-                        + PlainText(jie)
+                        +message.quote()
+                        +PlainText("[第 $randoms 签:解签]\r\r")
+                        +PlainText(jie)
                     }
                     group.sendMessage(chain)
-                }else{
+                } else {
                     val chain = buildMessageChain {
-                        + message.quote()
-                        + PlainText("请先使用抽签")
+                        +message.quote()
+                        +PlainText("请先使用抽签")
                     }
                     group.sendMessage(chain)
                 }
@@ -121,11 +117,11 @@ object QianMain : KotlinPlugin(
         QianUserTemp.reload()
         QianConfig.reload()
         val qian = getResource("qian.json")
-        if(qian is String){
+        if (qian is String) {
             QianConfig.qianList = Json.parseToJsonElement(qian)
-            QianConfig.error=false
-        }else{
-            QianConfig.error=true
+            QianConfig.error = false
+        } else {
+            QianConfig.error = true
             logger.error("配置文件加载失败")
         }
         val task = UserTempReload()
@@ -134,6 +130,7 @@ object QianMain : KotlinPlugin(
         // 计时器，默认每日23:59:59清空抽签缓存
         Timer().schedule(task, date, 86400000)
     }
+
     override fun onEnable() {
 
         QianUserTemp.reload()
